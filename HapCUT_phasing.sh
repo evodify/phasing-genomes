35bb74583ac949effade9f54e35e7d8ada0a9ec8
evodify <social7332@yahoo.com>
2016-09-23 16:15:27 +0200

    Update HapCUT_phasing.sh

--- HapCUT_phasing.sh
+++ HapCUT_phasing.sh
@@ -21,5 +21,5 @@ extractHAIRS --VCF sample1.vcf --bam sample1.bam --maxmem 128000 --mbq 20 --mmq 
 HAPCUT --fragments sample1.fragment_matrix --VCF sample1.vcf --output sample1.haplotype --maxiter 100 --maxmem 128000  > sample1.haplotype.log
 # --maxiter 100  and --maxiter 1000 produced the same results in my test.
 
-# In my data, phasing SNPs-only data produced haplotypes that were more consistent with parental reference genomes that phasing both SNPs and indels. 
-# to include indels, add options [--ref reference.fa --indels 1] to extractHAIRS command above
+# In my data, phasing SNPs-only data produced haplotypes that were more consistent with parental reference genomes than phasing both SNPs and indels. 
+# to include indels, add to extractHAIRS command above the following options: --ref reference.fa --indels 1
