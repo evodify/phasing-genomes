9f05082ff3edd8330a5d72e2c55a6851d2149fba
evodify <social7332@yahoo.com>
2016-09-24 21:14:45 +0200

    Update filterREFgenomeFixedOnly.py

--- filterREFgenomeFixedOnly.py
+++ filterREFgenomeFixedOnly.py
@@ -1,43 +1,39 @@
 #!/usr/bin/env python2
-##
-## This script filters two genomes reference by keeping only fixed differences.
-##
-## datafile:
-##
-## #CHROM  POS Species1  Species2
-## scaffold_1  519 G GTTCGTCCCGTGTGAGTTCTCATTCAGCCATATTGTCAACTAACTCTGCGATATTCTTCACTC
-## scaffold_1  528 * A,G
-## scaffold_1  531 * C,T
-## scaffold_1  574 * C,T
-## scaffold_1  580 * A,T
-## scaffold_1  585 C C,G
-## scaffold_1  619 TA  T
-## scaffold_1  640 T A,T
-## scaffold_1  641 A A,T
-## scaffold_1  644 A A,G
-## scaffold_1  647 C,G A
-##
-## outfile:
-##
-## scaffold_1  519 G GTTCGTCCCGTGTGAGTTCTCATTCAGCCATATTGTCAACTAACTCTGCGATATTCTTCACTC
-## scaffold_1  528 * A,G
-## scaffold_1  531 * C,T
-## scaffold_1  574 * C,T
-## scaffold_1  580 * A,T
-## scaffold_1  619 TA  T
-## scaffold_1  647 C,G A
-##
-## command:
-## $ python filterREFgenomeFixedOnly.py -i <datafile> -o <outputfile>
-##
-## contact Dmytro Kryvokhyzha dmytro.kryvokhyzha@evobio.eu
-##
-##*******************************************************************************************************
+#
+# This script filters two genomes reference by keeping only fixed differences.
+#
+# datafile:
+#
+# #CHROM  POS Species1  Species2
+# scaffold_1  519 G GTTCGTCCCGTGTGAGTTCTCATTCAGCCATATTGTCAACTAACTCTGCGATATTCTTCACTC
+# scaffold_1  528 * A,G
+# scaffold_1  531 * C,T
+# scaffold_1  574 * C,T
+# scaffold_1  580 * A,T
+# scaffold_1  585 C C,G
+# scaffold_1  619 TA  T
+# scaffold_1  640 T A,T
+# scaffold_1  641 A A,T
+# scaffold_1  644 A A,G
+# scaffold_1  647 C,G A
+#
+# outfile:
+#
+# scaffold_1  519 G GTTCGTCCCGTGTGAGTTCTCATTCAGCCATATTGTCAACTAACTCTGCGATATTCTTCACTC
+# scaffold_1  528 * A,G
+# scaffold_1  531 * C,T
+# scaffold_1  574 * C,T
+# scaffold_1  580 * A,T
+# scaffold_1  619 TA  T
+# scaffold_1  647 C,G A
+#
+# command:
+# $ python filterREFgenomeFixedOnly.py -i <datafile> -o <outputfile>
+#
+# contact Dmytro Kryvokhyzha dmytro.kryvokhyzha@evobio.eu
+#
+#*******************************************************************************************************
 import argparse, re, collections, sys
-  
-###################################################################
-############################# options #############################
-###################################################################
 
 class MyParser(argparse.ArgumentParser): 
    def error(self, message):
@@ -50,9 +46,6 @@ parser.add_argument('-i', '--input', help = 'name of the input file', type=str, 
 parser.add_argument('-o', '--output', help = 'name of the output file', type=str, required=True)
 args = parser.parse_args()
 
-####################################################################
-#########################   #######################
-####################################################################
 counter = 0
 print('Opening the file...')
 with open(args.input) as datafile:
