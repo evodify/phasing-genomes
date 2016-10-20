79e0408b8cff7a0634454a83882cde2c32c23d73
evodify <social7332@yahoo.com>
2016-10-20 16:42:21 +0200

    filterREFgenomeNonSharedOnly.py: annotated

--- filterREFgenomeNonSharedOnly.py
+++ filterREFgenomeNonSharedOnly.py
@@ -1,39 +1,41 @@
 #!/usr/bin/env python2
-#
-# This script filters two genomes reference by keeping only fixed differences.
-#
-# datafile:
-#
-# #CHROM  POS Species1  Species2
-# scaffold_1  519 G GTTCGTCCCGTGTGAGTTCTCATTCAGCCATATTGTCAACTAACTCTGCGATATTCTTCACTC
-# scaffold_1  528 * A,G
-# scaffold_1  531 * C,T
-# scaffold_1  574 * C,T
-# scaffold_1  580 * A,T
-# scaffold_1  585 C C,G
-# scaffold_1  619 TA  T
-# scaffold_1  640 T A,T
-# scaffold_1  641 A A,T
-# scaffold_1  644 A A,G
-# scaffold_1  647 C,G A
-#
-# outfile:
-#
-# scaffold_1  519 G GTTCGTCCCGTGTGAGTTCTCATTCAGCCATATTGTCAACTAACTCTGCGATATTCTTCACTC
-# scaffold_1  528 * A,G
-# scaffold_1  531 * C,T
-# scaffold_1  574 * C,T
-# scaffold_1  580 * A,T
-# scaffold_1  619 TA  T
-# scaffold_1  647 C,G A
-#
-# command:
-# $ python filterREFgenomeNonSharedOnly.py -i <datafile> -o <outputfile>
-#
-# contact Dmytro Kryvokhyzha dmytro.kryvokhyzha@evobio.eu
-#
-#*******************************************************************************************************
-import argparse, re, collections, sys
+'''
+This script filters two genomes reference by keeping only fixed differences.
+
+datafile:
+
+#CHROM  POS Species1  Species2
+scaffold_1  519 G GTTCGTCCCGTGTGAGTTCTCATTCAGCCATATTGTCAACTAACTCTGCGATATTCTTCACTC
+scaffold_1  528 * A,G
+scaffold_1  531 * C,T
+scaffold_1  574 * C,T
+scaffold_1  580 * A,T
+scaffold_1  585 C C,G
+scaffold_1  619 TA  T
+scaffold_1  640 T A,T
+scaffold_1  641 A A,T
+scaffold_1  644 A A,G
+scaffold_1  647 C,G A
+
+outfile:
+
+scaffold_1  519 G GTTCGTCCCGTGTGAGTTCTCATTCAGCCATATTGTCAACTAACTCTGCGATATTCTTCACTC
+scaffold_1  528 * A,G
+scaffold_1  531 * C,T
+scaffold_1  574 * C,T
+scaffold_1  580 * A,T
+scaffold_1  619 TA  T
+scaffold_1  647 C,G A
+
+command:
+$ python filterREFgenomeNonSharedOnly.py -i <datafile> -o <outputfile>
+
+contact Dmytro Kryvokhyzha dmytro.kryvokhyzha@evobio.eu
+'''
+
+import argparse, sys, re, collections
+
+############################ options ##############################
 
 class MyParser(argparse.ArgumentParser): 
    def error(self, message):
@@ -46,7 +48,10 @@ parser.add_argument('-i', '--input', help = 'name of the input file', type=str, 
 parser.add_argument('-o', '--output', help = 'name of the output file', type=str, required=True)
 args = parser.parse_args()
 
+############################ script ##############################
+
 counter = 0
+
 print('Opening the file...')
 with open(args.input) as datafile:
   header_line = datafile.readline()
@@ -58,16 +63,16 @@ with open(args.input) as datafile:
     words = line.split()
     GT1 = words[2].split(",")
     GT2 = words[3].split(",")
-    if bool(set(GT1) & set(GT2)):
-      continue
-    else:
+    
+    # check if there is overlap between two genomes
+    if not(bool(set(GT1) & set(GT2))): 
       output.write(line)
       
+    # track the progress:  
     counter += 1
     if counter % 1000000 == 0:
       print str(counter), "lines processed"
-    else:
-      continue
-  print('Done!\n')
+
 datafile.close()
 output.close()
+print('Done!\n')
